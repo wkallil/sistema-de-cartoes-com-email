70dd7d1ec4ccfa8bf4e6e7daf5972d0826ca9918
WKallil <kallil.tim@gmail.com>
2024-07-22 14:25:02 -0300

    iniciar projeto

---
+++ src/main/java/br/com/wkallil/cartoes/CartoesApplication.java
@@ -0,0 +1,13 @@
+package br.com.wkallil.cartoes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CartoesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CartoesApplication.class, args);
+	}
+
+}
